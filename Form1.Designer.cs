9da3fd7362070761e09ae66d574616a939fbcd7e
Alex Bollen <alexbollen21@gmail.com>
2024-04-21 19:24:51 -0600

    Main menu view

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,13 +23,25 @@
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -38,6 +50,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ MenuPrincipal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Administración_Centro_de_Convenciones {
+    public partial class MenuPrincipal : Form {
+        public MenuPrincipal() {
+            InitializeComponent();
+        }
+        bool btnReportsState = false;
+        private void btnClose_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e) {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void titleBar_Paint(object sender, PaintEventArgs e) {
+
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int IParam);
+
+        private void titleBar_MouseDown(object sender, MouseEventArgs e) {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnReports_Click(object sender, EventArgs e) {
+            if (!btnReportsState) {
+                reportsMenu.Visible = true;
+                btnReportsState = true;
+            } else if (btnReportsState) {
+                reportsMenu.Visible = false;
+                btnReportsState = false;
+            }
+        }
+
+        private void btnSpaces_Click(object sender, EventArgs e) {
+            reportsMenu.Visible = false;
+            btnReportsState = false;
+        }
+
+        private void btnActivities_Click(object sender, EventArgs e) {
+            reportsMenu.Visible = false;
+            btnReportsState = false;
+        }
+
+        private void btnOrganizers_Click(object sender, EventArgs e) {
+            reportsMenu.Visible = false;
+            btnReportsState = false;
+        }
+
+        private void btnCalendar_Click(object sender, EventArgs e) {
+            reportsMenu.Visible = false;
+            btnReportsState = false;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e) {
+            Form1 loginWindow = new Form1();
+            loginWindow.Show();
+            this.Dispose();
+        }
+
+        private void btnReports_MouseHover(object sender, EventArgs e) {
+        }
+
+        private void btnReports_MouseLeave(object sender, EventArgs e) {
+            
+        }
+    }
+}
