24e6816818826d11eaffabce072e0d366db312ad
Alex Bollen <alexbollen21@gmail.com>
2024-05-01 23:44:01 -0600

    Added insert new event feature

--- Reservas.cs
+++ Reservas.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 namespace Administración_Centro_de_Convenciones {
     public partial class Reservas : Form {
+        ClsEventos objEventos = new ClsEventos();
         public Reservas() {
             InitializeComponent();
         }
@@ -18,11 +19,20 @@ namespace Administración_Centro_de_Convenciones {
         private void button1_Click(object sender, EventArgs e) {
             listReservations.Hide();
             addReservation.Show();
+            btnAgregarReserva.Enabled = false;
+            btnEditarReserva.Enabled = false;
+            btnEliminarReserva.Enabled = false;
+            ListarSalones();
+            ListarOrganizadores();
+            ListarTiposEvento();
         }
 
         private void Reservas_Load(object sender, EventArgs e) {
             listReservations.Show();
             addReservation.Hide();
+            btnAgregarReserva.Enabled = true;
+            btnEditarReserva.Enabled = true;
+            btnEliminarReserva.Enabled = true;
             ListarEventos();
 
         }
@@ -30,11 +40,64 @@ namespace Administración_Centro_de_Convenciones {
         private void btnListarReservas_Click(object sender, EventArgs e) {
             listReservations.Show();
             addReservation.Hide();
+            btnAgregarReserva.Enabled = true;
+            btnEditarReserva.Enabled = true;
+            btnEliminarReserva.Enabled = true;
+            ListarEventos();
         }
 
         private void ListarEventos() {
-            ClsEventos objEventos = new ClsEventos();
-            dataGridView1.DataSource = objEventos.ListarEventos();
+            ClsEventos objListEventos = new ClsEventos();
+            dataGridView1.DataSource = objListEventos.ListarEventos();
+        }
+
+        private void ListarSalones() {
+            ClsEventos objSalones = new ClsEventos();
+            comboBoxSalon.DataSource = objSalones.ListarSalones();
+            comboBoxSalon.DisplayMember = "NombreSalon";
+            comboBoxSalon.ValueMember = "IdSalon";
+        }
+
+        private void ListarOrganizadores() {
+            ClsEventos objOrganizadores = new ClsEventos();
+            comboBoxOrganizadores.DataSource = objOrganizadores.ListarOrganizadores();
+            comboBoxOrganizadores.DisplayMember = "NombreOrganizador";
+            comboBoxOrganizadores.ValueMember = "IdOrganizador";
+        }
+
+        private void ListarTiposEvento() {
+            ClsEventos objListTE = new ClsEventos();
+            comboBoxTipoEvento.DataSource = objListTE.ListarTiposEvento();
+            comboBoxTipoEvento.DisplayMember = "NombreTipoEvento";
+            comboBoxTipoEvento.ValueMember = "IdTipoEvento";
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e) {
+            DateTime now = DateTime.Now;
+            int idItinerario = objEventos.InsertarItinerario(
+                dateTimePickerFI.Value, 
+                dateTimePickerFC.Value, 
+                txtBoxHoraInicio.Text, 
+                txtBoxHoraCulminacion.Text);
+            int idPersona= objEventos.InsertarPersona(
+                txtBoxNombreCliente.Text);
+            int idResponsable = objEventos.InsertarResponsable(
+                txtBoxNombreComercial.Text,
+                idPersona);
+            objEventos.InsertarReserva(
+                txtBoxNombreEvento.Text, 
+                txtBoxDescripcionEvento.Text,
+                "Programado",
+                Convert.ToInt32(txtBoxCantidadAsistentes.Text),
+                Convert.ToDateTime(now),
+                idItinerario,
+                idResponsable,
+                Convert.ToInt32(comboBoxOrganizadores.SelectedValue),
+                Convert.ToInt32(comboBoxTipoEvento.SelectedValue),
+                Convert.ToInt32(comboBoxSalon.SelectedValue)
+            );
+            MessageBox.Show("Insertado correctamente");
+            btnListarReservas.PerformClick();
         }
     }
 }
--- sql/ProcedimientosAlmacenados.sql
+++ sql/ProcedimientosAlmacenados.sql
@@ -23,3 +23,74 @@ INNER JOIN Salon ON e.IdSalon=Salon.IdSalon
 INNER JOIN TipoEvento ON e.IdTipoEvento=TipoEvento.IdTipoEvento
 
 GO
+
+CREATE PROC ListarSalones
+AS
+SELECT * FROM Salon ORDER BY IdSalon ASC
+GO
+
+CREATE PROC ListarOrganizadores
+AS
+SELECT o.IdOrganizador,
+		organizador.NombrePersona as NombreOrganizador
+FROM Organizador o 
+INNER JOIN Persona organizador ON o.IdOrganizador=organizador.IdPersona
+GO
+
+CREATE PROC ListarTipoEvento
+AS
+SELECT * FROM TipoEvento ORDER BY IdTipoEvento ASC
+GO
+
+CREATE PROC InsertarEvento
+@Nombre VARCHAR (100),
+@Descripcion VARCHAR(150),
+@EstadoEvento VARCHAR(75),
+@CantidadAsistentes INT,
+@FechaReserva DATE,
+@IdItinerario INT,
+@IdResponsable INT,
+@IdOrganizador INT,
+@IdTipoEvento INT,
+@IdSalon INT
+AS
+INSERT INTO Evento 
+VALUES (@Nombre, @Descripcion, @EstadoEvento, @CantidadAsistentes, @FechaReserva, @IdItinerario, @IdResponsable, @IdOrganizador, @IdTipoEvento, @IdSalon)
+GO
+
+CREATE PROC InsertarItinerario
+@FechaInicio DATE,
+@FechaCulminacion DATE,
+@HoraInicio VARCHAR(25),
+@HoraCulminacion VARCHAR(25),
+@IdItinerario INT OUTPUT
+AS
+BEGIN
+INSERT INTO Itinerario
+VALUES (@FechaInicio, @FechaCulminacion, @HoraInicio, @HoraCulminacion);
+SET @IdItinerario = SCOPE_IDENTITY();
+END;
+GO
+
+CREATE PROC InsertarPersona
+@NombrePersona VARCHAR(100),
+@IdPersona INT OUTPUT
+AS BEGIN
+	INSERT INTO Persona (NombrePersona)
+	VALUES (@NombrePersona);
+
+	SET @IdPersona = SCOPE_IDENTITY();
+END;
+GO
+
+CREATE PROC InsertarResponsable
+@NombreComercial VARCHAR(150),
+@IdPersona INT,
+@IdResponsableOutput INT OUTPUT
+AS BEGIN
+	INSERT INTO Responsable (NombreComercial, IdPersona)
+	VALUES (@NombreComercial, @IdPersona);
+
+	SET @IdResponsableOutput = SCOPE_IDENTITY();
+END;
+GO
